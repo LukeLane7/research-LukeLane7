ae7e45a76d58d5f6202089d231e2b448ddf4f1b2
Luke Lane <lukeag@email.sc.edu>
2024-10-13 19:19:27 -0400

    Added actually functionality

--- capstone-ex/public/formHandler.js
+++ capstone-ex/public/formHandler.js
@@ -17,9 +17,20 @@ async function submitForm(event) {
         const result = await response.json();
 
         document.getElementById('resultMessage').textContent = result.message;
+        document.getElementById('firstName').value = '';
+        document.getElementById('lastName').value = '';
+
+        addPlayerToList(firstName, lastName);
     // const message = document.getElementById('result');
     // message.textContent = result.message;
     } catch (error) {
         console.error('Error:', error);
     }
+}
+
+function addPlayerToList(firstName, lastName) {
+    const playerList = document.getElementById('playerList');
+    const li = document.createElement('li');
+    li.textContent = `${firstName} ${lastName}`;
+    playerList.appendChild(li);
 }

4c22413932c503b405ca87aa9969c2be874cef24
Luke Lane <lukeag@email.sc.edu>
2024-10-13 16:52:24 -0400

    Adding file progress to repository

---
+++ capstone-ex/index.js
@@ -0,0 +1,14 @@
+//LukeLane
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Lamp Chess!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
